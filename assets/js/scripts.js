bd859e427dac655b13a994f2ba6c7684207b862b
Vijay A T M <atmvijay1993@gmail.com>
2018-04-18 09:59:18 +0530

    change data type in script.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -329,7 +329,7 @@ $('#bootstrapForm').submit(function (event) {
     var extraData = {}
     $('#bootstrapForm').ajaxSubmit({
         data: extraData,
-        dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
+        // dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
         error: function () {
             // Submit of form should be successful but JSONP callback will fail because Google Forms
             // does not support it, so this is handled as a failure.
